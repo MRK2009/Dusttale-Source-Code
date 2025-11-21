d97abd06bc97d8409e125f45031978a3bb246130
Rafi <mohamedrafikhan2009@gmail.com>
2025-11-21 13:28:10 +0000

    Update ModchartState.hx

--- source/ModchartState.hx
+++ source/ModchartState.hx
@@ -4,7 +4,6 @@
 import openfl.display3D.textures.VideoTexture;
 import flixel.graphics.FlxGraphic;
 import flixel.graphics.frames.FlxAtlasFrames;
-#if windows
 import flixel.tweens.FlxEase;
 import openfl.filters.ShaderFilter;
 import flixel.tweens.FlxTween;
@@ -439,41 +438,6 @@ class ModchartState
 				});
 	
 				// hud/camera
-
-				Lua_helper.add_callback(lua,"initBackgroundVideo", function(videoName:String) {
-					trace('playing assets/videos/' + videoName + '.webm');
-					PlayState.instance.backgroundVideo("assets/videos/" + videoName + ".webm");
-				});
-
-				Lua_helper.add_callback(lua,"pauseVideo", function() {
-					if (!GlobalVideo.get().paused)
-						GlobalVideo.get().pause();
-				});
-
-				Lua_helper.add_callback(lua,"resumeVideo", function() {
-					if (GlobalVideo.get().paused)
-						GlobalVideo.get().pause();
-				});
-				
-				Lua_helper.add_callback(lua,"restartVideo", function() {
-					GlobalVideo.get().restart();
-				});
-
-				Lua_helper.add_callback(lua,"getVideoSpriteX", function() {
-					return PlayState.instance.videoSprite.x;
-				});
-
-				Lua_helper.add_callback(lua,"getVideoSpriteY", function() {
-					return PlayState.instance.videoSprite.y;
-				});
-
-				Lua_helper.add_callback(lua,"setVideoSpritePos", function(x:Int,y:Int) {
-					PlayState.instance.videoSprite.setPosition(x,y);
-				});
-				
-				Lua_helper.add_callback(lua,"setVideoSpriteScale", function(scale:Float) {
-					PlayState.instance.videoSprite.setGraphicSize(Std.int(PlayState.instance.videoSprite.width * scale));
-				});
 	
 				Lua_helper.add_callback(lua,"setHudAngle", function (x:Float) {
 					PlayState.instance.camHUD.angle = x;
@@ -915,4 +879,3 @@ class ModchartState
         return new ModchartState();
     }
 }
-#end
